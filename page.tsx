a91fd9f256905b5e0fa377dfa5c8a2e042d4d837
Makanmand-dev <mehranjamoosi@gmail.com>
2025-05-23 14:51:33 +0330

    ????? ???? ???? ???? ???? ??? 404

---
+++ page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>سلام مکانمند!</h1>
+      <p>پروژه با موفقیت Deploy شده ✅</p>
+    </main>
+  );
+}
