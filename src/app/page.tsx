0fb354deea4145c5b77b1d3b2e2f14c94b44ba90
Makanmand-dev <mehranjamoosi@gmail.com>
2025-05-24 19:14:00 +0330

    dobareh - 3 khordad 1404

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ export default function Home() {
   return (
     <main style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
       <h1>سلام مکانمند!</h1>
-      <p>پروژه-- مکانمند -- با موفقیت Deploy شده ✅</p>
+      <p>پروژه-+- مکانمند -+- با موفقیت Deploy شده ✅</p>
     </main>
-  );
-}
+  );}
