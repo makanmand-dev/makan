98e067de6fb378c75dfd41ddbe0bf2881658ed08
Makanmand-dev <mehranjamoosi@gmail.com>
2025-05-24 21:26:34 +0330

    test nahaei-2-3-404--

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <main style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
-      <h1>سلام مکانمند!</h1>
-      <p>پروژه-+- مکانمند -+- با موفقیت Deploy شده ✅</p>
-    </main>
-  );}
+    <main>
+      <h1>سلام مهران مکانمند</h1>
+         </main>
+  );
+}
