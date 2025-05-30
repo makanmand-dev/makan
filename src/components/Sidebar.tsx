e07518311f305d0d019f8c8fdfa7c666a731a5f6
mehranjim <mehranjamoosi@gmail.com>
2025-05-30 06:48:07 +0000

    2222222222222222222

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,13 @@
+export default function Sidebar() {
+  return (
+    <div className="sidebar">
+      <div className="handle">≡</div>
+      <h3>پنل کاربر</h3>
+      <ul>
+        <li>ورود / ثبت‌نام</li>
+        <li>تنظیمات</li>
+        <li>ملک‌های من</li>
+      </ul>
+    </div>
+  );
+}
